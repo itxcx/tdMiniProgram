9418adf51f71318f9585530e0ec8f8b5a903b927
lujunwei <644480352@qq.com>
2017-04-14 11:28:34 +0800

    小程序首页，登录页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,22 @@
-//index.js
 //获取应用实例
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    isLogined: false,
+    assetInfo: {
+      total: '--',
+      receivedIncome: '--'
+    }
   },
-  //事件处理函数
-  bindViewTap: function() {
+  //跳转至登录页面
+  goLogin: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../user/login'
     })
   },
+
   onLoad: function () {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+   
   }
 })
---
+++ pages/user/login.js
@@ -0,0 +1,91 @@
+
+Page({
+  data: {
+    isShowPwd: false,
+    isLogining: false,
+    username: '',
+    pwd: ''
+  },
+  // 用户名输入绑定事件，获得用户名
+  onUserNameInput: function(e) {
+    this.setData({
+      username: e.detail.value
+    });
+  },
+  // 密码输入绑定事件，获得密码
+  onPwdInput: function(e) {
+    this.setData({
+      pwd: e.detail.value
+    });
+  },
+  // 是否显示密码
+  onToggleShowPwd: function(e) {
+    let isShowPwd = false;
+
+    // 获取类型
+    let type = e.currentTarget.dataset && e.currentTarget.dataset.type;
+
+    // 点击显示密码
+    if('off' === type) isShowPwd = true;
+
+    // 设置数据
+    this.setData({
+      isShowPwd: isShowPwd
+    });
+  },
+  
+  // 登录
+  onLogin: function() {
+    let username = this.data.username;
+    let pwd = this.data.pwd;
+
+    // 用户名为空
+    if('' === username) {
+      wx.showToast({
+        title: '请输入用户名'
+      });
+
+      return;
+    }
+
+    //密码为空 
+    if('' === pwd) {
+      wx.showToast({
+        title: '请输入密码'
+      });
+
+      return;
+    } else if(pwd.length < 6) {
+      wx.showToast({
+        title: '密码至少应包含6位字符'
+      });
+      
+     return;
+    }
+
+    // 设置数据
+    this.setData({
+      isLogining: true
+    });
+    // TODO:请求登录接口
+    // wx.request();
+  },
+
+  //跳转至注册页面
+  goRegister: function() {
+    wx.navigateTo({
+      url: '../user/registe'
+    })
+  },
+
+  //跳转至忘记密码页面 
+  goResetPwd: function() {
+    wx.navigateTo({
+      url: '../user/resetpwd'
+    })
+  },
+
+  onLoad: function () {
+    
+  }
+})
