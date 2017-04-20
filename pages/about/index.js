0e0d6eee1bce5eba222cadb181c7856a35fd3f86
lujunwei <644480352@qq.com>
2017-04-20 17:37:03 +0800

    了解团贷网部分页面

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,7 +2,26 @@
 var app = getApp()
 Page({
   data: {
-	isShowMore: false
+	isShowMore: false,
+	partners: [{
+		img: '../../images/about_gd1.png',
+		desc: '“中国私募股权投资机构50强”第一名'
+	},{
+		img: '../../images/about_gd2.png',
+		desc: '商界传奇人物史玉柱先生创办建立'
+	},{
+		img: '../../images/about_gd3.png',
+		desc: '中国创业投资机构综合排名19强'
+	},{
+		img: '../../images/about_gd4.png',
+		desc: '全国首批承销保荐机构之一'
+	},{
+		img: '../../images/about_gd5.png',
+		desc: '高成长潜力股的创投机构'
+	},{
+		img: '../../images/about_gd6.png',
+		desc: '国家高新技术企业'
+	}]
   },
   onToggleText: function() {
   	this.setData({
@@ -10,8 +29,13 @@ Page({
     });
   },
   goMoreBackground: function() {
-  	 wx.navigateTo({
+  	wx.navigateTo({
       url: '../about/background'
+    });
+  },
+  goPartners:  function() {
+	wx.navigateTo({
+      url: '../about/partners'
     });
   }
 })
---
+++ pages/about/partners.js
@@ -0,0 +1,44 @@
+Page({
+	data: {
+		partners: [{
+		img: '../../images/about_partner1.png',
+		name: '巨人投资'
+	},{
+		img: '../../images/about_partner2.png',
+		name: '九鼎投资'
+	},{
+		img: '../../images/about_partner3.png',
+		name: '久奕投资'
+	},{
+		img: '../../images/about_partner4.png',
+		name: '达商小贷'
+	},{
+		img: '../../images/about_partner5.png',
+		name: '百度'
+	},{
+		img: '../../images/about_partner6.png',
+		name: '正合普惠'
+	},{
+		img: '../../images/about_partner7.png',
+		name: '芝麻信用'
+	},{
+		img: '../../images/about_partner8.png',
+		name: '分众传媒'
+	},{
+		img: '../../images/about_partner9.png',
+		name: '财付通'
+	},{
+		img: '../../images/about_partner10.png',
+		name: '俊拓金融'
+	},{
+		img: '../../images/about_partner11.png',
+		name: '宝付支付'
+	},{
+		img: '../../images/about_partner12.png',
+		name: '连连支付'
+	},{
+		img: '../../images/about_partner13.png',
+		name: '厦门银行'
+	}]
+	}
+})
