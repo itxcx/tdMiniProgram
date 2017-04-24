6180bb8bc4a70f5087cdb020865e1c3c4420e6a4
lujunwei <644480352@qq.com>
2017-04-24 14:20:42 +0800

    整合团贷网合作伙伴页面

--- pages/about/partners.js
+++ pages/about/partners.js
@@ -1,44 +1,43 @@
+let util = require('../../utils/util.js');
 Page({
 	data: {
-		partners: [{
-		img: '../../images/about_partner1.png',
-		name: '巨人投资'
-	},{
-		img: '../../images/about_partner2.png',
-		name: '九鼎投资'
-	},{
-		img: '../../images/about_partner3.png',
-		name: '久奕投资'
-	},{
-		img: '../../images/about_partner4.png',
-		name: '达商小贷'
-	},{
-		img: '../../images/about_partner5.png',
-		name: '百度'
-	},{
-		img: '../../images/about_partner6.png',
-		name: '正合普惠'
-	},{
-		img: '../../images/about_partner7.png',
-		name: '芝麻信用'
-	},{
-		img: '../../images/about_partner8.png',
-		name: '分众传媒'
-	},{
-		img: '../../images/about_partner9.png',
-		name: '财付通'
-	},{
-		img: '../../images/about_partner10.png',
-		name: '俊拓金融'
-	},{
-		img: '../../images/about_partner11.png',
-		name: '宝付支付'
-	},{
-		img: '../../images/about_partner12.png',
-		name: '连连支付'
-	},{
-		img: '../../images/about_partner13.png',
-		name: '厦门银行'
-	}]
+		partners: []
+	},
+	onLoad: function() {
+		let that = this;
+		let timestamp = +new Date();
+		let apiToken = util.md5('tuandai_xcx' + timestamp);
+
+		wx.showLoading({
+			title: '加载中',
+			mask: true
+		});
+
+		util.request({
+			url: util.domain + '/about/partners.html',
+			data: {
+				t: timestamp,
+				api_token: apiToken
+			},
+			success: function(res){
+				let data = res.data;
+				if(200 === data.code) {
+
+					console.log( data.data);
+					that.setData({
+				      partners: data.data
+				    });
+				} else {
+					// TODO: taost res.msg
+				}
+			},
+			fail: function(err) {
+				// TODO: taost 网络异常，请稍后再试
+			},
+			complete: function() {
+				wx.hideLoading()
+			}
+
+		});
 	}
 })
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,4 @@
+let md5 = require('./md5.js');
 function formatTime(date) {
   var year = date.getFullYear()
   var month = date.getMonth() + 1
@@ -21,7 +22,25 @@ function checkPhoneNum(number) {
   let reg = /^1(3|4|5|7|8)\d{9}$/;
   return reg.test(number) ? true : false;
 }
+
+// 请求数据
+function request(params) {
+  // 设置header的contentType
+  params.header = params.header || {};
+  params.header['content-type'] = params.header['content-type'] || 'application/x-www-form-urlencoded';
+  
+  params.method = params.method || 'POST';
+
+  wx.request(params)
+}
+
+// 请求域名
+let domain = 'https://bbs5.tuandai.com';  //dev
+
 module.exports = {
   formatTime: formatTime,
-  checkPhoneNum: checkPhoneNum
+  checkPhoneNum: checkPhoneNum,
+  request: request,
+  domain: domain,
+  md5: md5
 }
